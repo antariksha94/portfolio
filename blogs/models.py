34a2483cfc2b07592994be26baaa5a92ef667ad1
antariksha <antariksha94@gmail.com>
2020-08-27 02:58:30 +0530

    Portfolio site completed

--- blogs/models.py
+++ blogs/models.py
@@ -5,3 +5,9 @@ class Blogs(models.Model):
 	pub_date= models.DateField()
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
+
+	def __str__(self):
+		return self.title
+
+	def summary(self):
+		return self.body[:100]
